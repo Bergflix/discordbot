613e6fcbfd3da53116d5fff84c36adb70b0416ba
RyanFlm <info@apelma.de>
2020-05-27 21:52:57 +0200

    Simplified clear command

--- src/classes/commands/Clear.ts
+++ src/classes/commands/Clear.ts
@@ -12,20 +12,8 @@ class Clear extends Command {
     }
 
     async exec(data: CommandData): Promise<void> {
-        let ret = "Dieser Channel wurde geleert und ist bereit für neue Aufgaben.";
-
-        let list = await data.channel.cnl.messages.cache.filter(msg => msg.createdAt.getDate() === new Date("14 days ago").getDate());
-
-        if (data.args[0] !== "force") {
-            await data.channel.handler.Channel.bulkDelete(list);
-        } else {
-            list.forEach(item => {
-                item.delete().catch(e => console.error("Error", e));
-            });
-            ret = "\n**WARNUNG:** Es ist möglich, dass es noch weitere Nachrichten gibt, die nicht mit **force** gelöscht werden konnten!";
-        }
-
-        data.channel.handler.sendInfo(ret, "Channel geleert");
+        await data.channel.handler.Channel.bulkDelete(100);
+        data.channel.handler.sendInfo(`Dieser Channel wurde von <@${data.user.id}> geleert.`, "Channel geleert");
     }
 }
 
