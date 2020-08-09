a060fb4717a415f3de94ac748a5fdc3562fe4c7d
6lyxt <dundeeserver39@gmail.com>
2020-08-09 23:11:53 +0200

    Update Tech-Support.ts

--- src/classes/commands/Tech-Support.ts
+++ src/classes/commands/Tech-Support.ts
@@ -24,7 +24,7 @@ class Help extends Command {
                 data.channel.handler.sendInfo(":tools:FTF Tech Support:tools: .\nLeider erreichen sie uns gerade außerhalb unserer Geschäftszeiten. Schreiben sie uns ihr anliegen doch gerne Montags bis Freitags von 9:00 Uhr bis 18:30 Uhr.\nSollten sie dringende Anliegen haben, so wenden sie sich doch bitte an die Praktikanten dieses Servers. Wir freuen uns auf ihre Nachricht, und bedanken uns für ihre Aufmerksamkeit! \nUnd denken sie immer daran: IHRE Probleme sind UNSER Verdienst.", "Support geschlossen.")
             }
         } else {
-            data.channel.handler.sendInfo(":tools:FTF Tech Support:tools:\n*Zum glück erreichen sie uns gerade innerhalb unserer Geschäftszeiten!*\nSenden sie bitte ihr Anliegen per Privatnachricht an: @Nikasukee#0001.\nDer genannte Mitarbeiter wird sich dann nach ausgiebiger Fehlerrecherche bei ihnen zur Problembehandlung melden!\n**Wir bedanken uns für ihre Aufmerksamkeit!**")
+            data.channel.handler.sendInfo(":tools:FTF Tech Support:tools:\n*Zum glück erreichen sie uns gerade innerhalb unserer Geschäftszeiten!*\nSenden sie bitte ihr Anliegen per Privatnachricht an: @Nikasukee#0001.\nDer genannte Mitarbeiter wird sich dann nach ausgiebiger Fehlerrecherche bei ihnen zur Problembehandlung melden!\n**Wir bedanken uns für ihre Aufmerksamkeit!**", "Support geöffnet!")
         }
     }
 }
